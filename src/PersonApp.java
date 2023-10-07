13543f5ac604d2e714480882319aad0455dc02a5
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-10-07 11:13:01 +0700

    fix: run object

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,7 +1,7 @@
 public class PersonApp {
-    public static main(String[] args) {
+    public static void main(String[] args) {
         
-        var person1 = new Person();
+        Person person1 = new Person();
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
