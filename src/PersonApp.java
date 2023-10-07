343a302a6267a95635968f69182e72f95e83bed9
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-10-07 11:49:06 +0700

    feat: field

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,13 +2,15 @@ public class PersonApp {
     public static void main(String[] args) {
         
         Person person1 = new Person();
+        person1.name = "Dede";
+        person1.address = "Jakarta";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-
     }
 }
