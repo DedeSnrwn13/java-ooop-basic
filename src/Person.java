96ae2caa28aa3cbc79d760ab74af565f0e2e3c8f
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-10-07 11:54:58 +0700

    feat: method

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,8 @@ class Person {
     String name;
     String address;
     final String country = "Indoneisa";
+
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -9,8 +9,13 @@ public class PersonApp {
         System.out.println(person1.address);
         System.out.println(person1.country);
 
+        person1.sayHello("Joko");
+
         Person person2 = new Person();
+
         Person person3;
         person3 = new Person();
+        person3.name = "Andi";
+        person3.sayHello("Anggi");
     }
 }
