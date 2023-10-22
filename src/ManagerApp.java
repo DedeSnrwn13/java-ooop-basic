03cd695fc32fb861b2a2d5dc7f01b4ef0ce64a96
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-10-22 10:08:38 +0700

    feat: super constructor

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,12 +1,10 @@
 public class ManagerApp {
     public static void main(String[] args) {
         
-        Manager manager = new Manager();
-        manager.name = "Dede";
+        Manager manager = new Manager("Dede");
         manager.sayHello("Budi");
 
-        VicePresident vp = new VicePresident();
-        vp.name = "Joko";
+        VicePresident vp = new VicePresident("Joko");
         vp.sayHello("Rudi");
 
     }
