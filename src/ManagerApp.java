4184a06f220f652dd950094684c36dab8874858d
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-10-22 10:14:05 +0700

    feat: object class

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -7,5 +7,10 @@ public class ManagerApp {
         VicePresident vp = new VicePresident("Joko");
         vp.sayHello("Rudi");
 
+
+        System.out.println(manager);
+        System.out.println(manager.toString());
+        System.out.println(vp);
+        System.out.println(vp.toString());
     }
 }
